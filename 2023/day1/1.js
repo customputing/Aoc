7d3300358426f3205ddcde833c979c939bc2c666
customputing <58713233+customputing@users.noreply.github.com>
2023-12-01 11:07:15 -0600

    saved match

--- 2023/day1/1.js
+++ 2023/day1/1.js
@@ -7,13 +7,11 @@ require('fs').readFile('day1\\values.txt', (err, data) => {
     if(err) throw err;
     var sum = 0;
     var values = data.toString().split('\n');
-    for(var i = 0; i < data.length; i++){
-        if(values[i]){
-            let d = 0
-            d = values[i].match(/\d+/g).at(0).at(0).toString()
-            d += (values[i].match(/\d+/g).at(-1).at(-1).toString())
-            sum += parseInt(d);
-        }
+    for(var i = 0; i < values.length - 1; i++){
+        var matched = values[i].match(/\d+/g)
+        d = matched.at(0).at(0).toString()
+        d += matched.at(-1).at(-1).toString()
+        sum += parseInt(d);
     }
     console.log(sum);
 })
